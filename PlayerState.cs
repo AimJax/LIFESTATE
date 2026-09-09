bf7130c455a446546d96258a2f5ee050c4dfa0cb
AimJax <rizkyviriano024@gmail.com>
2026-09-09 09:12:59 +0700

    Initial project setup: PlayerState.cs, Program.cs, Lifestate.csproj

---
+++ PlayerState.cs
@@ -0,0 +1,6 @@
+public class PlayerState
+{
+    public int Age { get; set; } = 22;
+    public int Money { get; set; } = 1000;
+    public int Energy { get; set; } = 100;
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+var player = new PlayerState();
+
+Console.WriteLine($"Age: {player.Age}");
+Console.WriteLine($"Money: {player.Money}");
+Console.WriteLine($"Energy: {player.Energy}");
